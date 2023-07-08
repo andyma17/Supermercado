1b4fa64a8555673a5c921edd11513ed0957f13f3
andyma17 <101306238+andyma17@users.noreply.github.com>
2023-07-07 20:18:53 -0600

    Se crea la clase controller
    Categoria
    Producto y pruebas

---
+++ supermercadoo/src/main/java/com/supermercado/controller/ProductoController.java
@@ -0,0 +1,62 @@
+package com.supermercado.controller;
+
+
+import com.supermercado.domain.Categoria;
+import com.supermercado.domain.Producto;
+import com.supermercado.service.CategoriaService;
+import com.supermercado.service.FirebaseStorageService;
+import com.supermercado.service.impl.FirebaseStorageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.supermercado.service.ProductoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/producto")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var productos = productoService.getProductos(false);
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("categorias", categorias);
+        return "/producto/listado";
+    }
+
+    @GetMapping("/listado/{idCategoria}")
+    public String listado(Model model, Categoria categoria) {
+        var productos = categoriaService.getCategoria(categoria).getProductos();
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("categorias", categorias);
+        return "/producto/listado";
+    }
+
+     @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+    
+    @PostMapping("/guardar")
+    public String productoGuardar(Producto producto,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+        if (!imagenFile.isEmpty()) {
+            productoService.save(producto);
+            producto.setRutaImagen(
+            firebaseStorageService.cargaImagen(imagenFile, "producto", producto.getIdProducto()));
+        }
+        productoService.save(producto);
+        return "redirect:/producto/listado";
+    }
+}
