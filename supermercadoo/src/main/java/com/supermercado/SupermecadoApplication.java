0107aaff75451f1cebab526072872b2ed57ecd40
andyma17 <101306238+andyma17@users.noreply.github.com>
2023-07-07 20:10:09 -0600

    Add files via upload

---
+++ supermercadoo/src/main/java/com/supermercado/SupermecadoApplication.java
@@ -0,0 +1,13 @@
+package com.supermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermecadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermecadoApplication.class, args);
+	}
+
+}
