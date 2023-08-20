58dd0535beff8aecacbc56b88fe5a7e37be4b896
andyma17 <101306238+andyma17@users.noreply.github.com>
2023-08-20 15:30:08 -0600

    Update ProductoDao.java
    Actualizacion DaoProducto

--- supermercadoo/src/main/java/com/supermercado/dao/ProductoDao.java
+++ supermercadoo/src/main/java/com/supermercado/dao/ProductoDao.java
@@ -1,11 +1,19 @@
-package com.supermercado.dao;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.IsariSupermercado.dao;
 
-import com.supermercado.domain.Producto;
+import com.IsariSupermercado.domain.Producto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+/**
+ *
+ * @author Usuario
+ */
 public interface ProductoDao extends JpaRepository<Producto, Long> {
     //Ejemplo de método utilizando Métodos de Query
 
